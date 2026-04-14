626e6dd1af8f9c964e37964d9dc311dd3fc2383a
kelvyn2012 <kudufo@gmail.com>
2026-04-14 11:05:02 +0100

    feat: add profile serializer

---
+++ api/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Profile
+
+class ProfileSerializer(serializers.ModelSerializer):
+    created_at = serializers.DateTimeField(format="%Y-%m-%dT%H:%M:%SZ", read_only=True)
+
+    class Meta:
+        model = Profile
+        fields = [
+            'id', 'name', 'gender', 'gender_probability', 
+            'sample_size', 'age', 'age_group', 
+            'country_id', 'country_probability', 'created_at'
+        ]
